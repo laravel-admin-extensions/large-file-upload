5e06c4a39f7ddd24c4abb6685c059e20e49e26a1
dianwang <dian126@foxmail.com>
2018-10-29 16:00:53 +0800

    新增了安装说明文本

--- src/LargeFileUpload.php
+++ src/LargeFileUpload.php
@@ -8,6 +8,8 @@ class LargeFileUpload extends Extension
 {
     public $name = 'large-file-upload';
 
+    public $views = __DIR__.'/../resources/views';
+
     public $assets = __DIR__.'/../resources/assets';
 
 }
